6b28e190aad7ded13dc417373c139a6150ad3f94
Andres Martinez <a.martinez@raemexico.mx>
2020-12-10 18:01:12 -0600

    Se cambio clase operaciones a fracciones.js y se agrego un html para hacer pruebas de operaciones

---
+++ js/fracciones.js
@@ -0,0 +1,12 @@
+// Pagina de operaciones
+
+
+function multiplicar(num1, num2){
+    resultado=num1*num2
+    return resultado
+}
+
+function dividir(num1,num2){
+    resultado=num1/num2
+    return resultado
+}
