e6d92a44fbc1be716ce9143ee2af1a63decab922
Andres Martinez <a.martinez@raemexico.mx>
2020-12-11 00:54:57 -0600

    Se han agregado operaciones con fracciones en fracciones.js

---
+++ js/fracciones.js
@@ -0,0 +1,47 @@
+// Pagina de operaciones
+function sumar(numerador1,denominador1, numerador2, denominador2 ){
+    let resultadoNumerador=0
+    let resultadoDenominador=0
+    if(denominador1==denominador2){
+        resultadoNumerador=numerador1 + numerador2
+        resultadoDenominador=denominador1 + denominador2
+    }
+    else{
+        resultadoNumerador1=numerador1 * denominador2
+        resultadoNumerador2=numerador2 * denominador1
+        resultadoDenominador=denominador1*denominador2
+        resresNumerador=resultadoNumerador1 + resultadoNumerador2
+    }
+    return resresNumerador, resultadoDenominador 
+}
+
+function restar(numerador1,denominador1, numerador2, denominador2 ){
+    let resultadoNumerador=0
+    let resultadoDenominador=0
+    if(denominador1==denominador2){
+        resultadoNumerador=numerador1 + numerador2
+        resultadoDenominador=denominador1 + denominador2
+    }
+    else{
+        resultadoNumerador1=numerador1 * denominador2
+        resultadoNumerador2=numerador2 * denominador1
+        resultadoDenominador=denominador1*denominador2
+        resresNumerador=resultadoNumerador1 - resultadoNumerador2
+    }
+    return resresNumerador, resultadoDenominador 
+}
+
+function multiplicar(numerador1,denominador1, numerador2, denominador2 ){
+    resultado=num1*num2
+    return resultado
+}
+
+function dividir(numerador1,denominador1, numerador2, denominador2 ){
+    resultado=num1/num2
+    return resultado
+}
+
+function (){
+
+}
+
