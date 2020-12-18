89dcd45f8a08b3accb5c737d2ae069b5030a9d17
Andres Martinez <a.martinez@raemexico.mx>
2020-12-18 15:05:48 -0600

    Se han agregado mas funciones en fracciones, se han agregado todas las restantes, amplificacion, conversion, reduccion, representacion, falta hacer explicacion y disenio

--- js/fracciones.js
+++ js/fracciones.js
@@ -92,9 +92,9 @@ function dividir(){
 }
 
 
-function representar(numerador1, denominador1){
-    return numerador1, denominador1
-}
+// function representar(numerador1, denominador1){
+//     return numerador1, denominador1
+// }
 
 function mayorMenorque(numerador1,denominador1, numerador2, denominador2 ){
     let fraccionizq, fraccionderecha
@@ -113,7 +113,6 @@ function mayorMenorque(numerador1,denominador1, numerador2, denominador2 ){
     return mensaje
 }
 function representar(){
-    dibujarPastel()
     let numerador=parseInt(document.getElementById("reprenumerador").value)
     let denominador=parseInt(document.getElementById("repredenominador").value)
     if (denominador<=numerador){
@@ -124,6 +123,8 @@ function representar(){
         document.getElementById("repreExplicacion").innerHTML="El numero entero se representa del lado izquierdo"
         console.log("El numerador es mayor", Math.trunc(res))
     }else{
+        dibujarPastel()
+
         document.getElementById("rreprenumerador").innerHTML=parseInt(numerador)
         document.getElementById("rrepredenominador").innerHTML=parseInt(denominador)
         document.getElementById("repreExplicacion").innerHTML="Numerador menor que el denominador"
@@ -263,6 +264,8 @@ function dibujarPastel(){
             for(var i in this.valores)
             {
                 total+=valores[i]["valor"];
+                // total=parseInt(document.getElementById("repredenominador").value)
+
             }
             return total;
         }
@@ -291,14 +294,17 @@ function dibujarPastel(){
     //     ".NET":{valor:12,color:"Orange"},
     //     "C++":{valor:30,color:"Cyan"}
     // }
-    let x=parseInt(document.getElementById("reprenumerador").value)
-    let y=parseInt(document.getElementById("repredenominador").value)
+    let x=document.getElementById("reprenumerador").value
+    let y=document.getElementById("repredenominador").value
+    let sobrante=1-x/y
+    let fraccion=x/y
+    console.log("sobrante",sobrante,"fraccion",fraccion, x, y)
     var valores={
         // "Access":{valor:10,color:"blue"},
         // "PHP":{valor:20,color:"red"}
 
-        "parte tomada(numerador)":{valor:x,color:"blue"},
-        "parte restante":{valor:y,color:"red"}
+        "parte tomada(numerador)":{valor:sobrante,color:"blue"},
+        "parte restante":{valor:fraccion,color:"red"}
     }
      
     // generamos el primer pastel
@@ -324,3 +330,104 @@ function dibujarPastel(){
          document.getElementById("rmixtonumerador").innerHTML=resnumerador
          document.getElementById("rmixtodenominador").innerHTML=resdenominador
      }
+
+     function comparar(){
+         let numerador1=parseInt(document.getElementById("compnumerador1").value)
+         let numerador2=parseInt(document.getElementById("compnumerador2").value)
+         let denominador1=parseInt(document.getElementById("compdenominador1").value)
+         let denominador2=parseInt(document.getElementById("compdenominador2").value)
+         let comparacion=document.getElementById("comp").value
+         ter1=numerador1*denominador2
+         ter2=numerador2*denominador1
+         
+         switch(comparacion){
+
+            case '>':
+                if(ter1>ter2){
+                    document.getElementById("compExplicacion").innerHTML="Es correcto"
+                }else if(ter1==ter2){
+                    document.getElementById("compExplicacion").innerHTML="Las fracciones equivalen lo mismo"
+                }else{
+                    document.getElementById("compExplicacion").innerHTML="La comparacion es incorrecta"
+                }
+                console.log("Entro en el case")
+                break;
+            case '<':
+                if(ter1<ter2){
+                    document.getElementById("compExplicacion").innerHTML="Es correcto"
+                }else if(ter1==ter2){
+                    document.getElementById("compExplicacion").innerHTML="Las fracciones equivalen lo mismo"
+                }else{
+                    document.getElementById("compExplicacion").innerHTML="La comparacion es incorrecta"
+                }
+                console.log("Entro en el case")
+                break;
+            case '=':
+                if(ter1==ter2){
+                    document.getElementById("compExplicacion").innerHTML="Es correcto"
+                }else{
+                    document.getElementById("compExplicacion").innerHTML="No son equivalentes "+ter1+" y "+ter2
+
+                }
+                break;
+                
+            default:
+                document.getElementById("compExplicacion").innerHTML="Ingreso un caracter erroneo"
+                console.log("No entro en el caso",ter1,ter2,comparacion)
+
+        }
+
+
+
+     }
+
+     function convertirdecimal(){
+         let numerador=parseInt(document.getElementById("convnumerador").value)
+         let denominador=parseInt(document.getElementById("convdenominador").value)
+         res=numerador/denominador
+         document.getElementById("convExplicacion").innerHTML=res+" Es la equivalencia con punto decimal"
+
+     }
+
+     function simplificar(){
+        let numerador=parseInt(document.getElementById("simnumerador").value)
+        let denominador=parseInt(document.getElementById("simdenominador").value)
+            
+        
+     }
+
+     //////////////////////////////////////////////
+
+    function EuclidesMCD() {
+    a=document.getElementById("simnumerador").value
+    b=document.getElementById("simdenominador").value
+    var iaux; //auxiliar
+    a = Math.abs(a); //tomamos valor absoluto
+    b = Math.abs(b);
+    var i1 = Math.max(a, b); //i1 = el más grande
+    var i2 = Math.min(a, b); //i2 = el más pequeño
+
+    do {
+        iaux = i2; //guardar divisor
+        i2 = i1 % i2; //resto pasa a divisor
+        i1 = iaux; //divisor pasa a dividendo
+    } while (i2 !== 0);
+    // document.getElementById("rMCD").innerHTML=i1
+    rnumerador=a/parseInt(i1)
+    rdenominador=b/parseInt(i1)
+    document.getElementById("rsimnumerador").innerHTML=rnumerador
+    document.getElementById("rsimdenominador").innerHTML=rdenominador
+    document.getElementById("simExplicacion").innerHTML="tiene un Maximo comun divisor que es "+i1+" quedando como resultado "
+    return i1; //ultimo resto no nulo
+    }
+
+     //////////////////////////////////////////////
+     function Amplificar(){
+         let amplificador=parseInt(document.getElementById("amp").value)
+         let numerador=parseInt(document.getElementById("ampnumerador").value)
+         let denominador=parseInt(document.getElementById("ampdenominador").value)
+
+         document.getElementById("rampExplicacion").innerHTML='<sup><label type="number" id="ampnumerador" >'+numerador*amplificador+'</label></sup> / <sub> <label type="number" id="ampdenominador">'+denominador*amplificador+'</label> </sub>'
+
+     }
+      
